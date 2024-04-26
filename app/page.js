42704d0c160afc6b1b85c4c7c5d65b74b6eba7a6
Justin Dumadag <justin.dumadag@gmail.com>
2024-04-26 18:31:50 -0400

    home page changes

--- app/page.js
+++ app/page.js
@@ -1,20 +1,39 @@
 import Image from 'next/image';
-import styles from './page.module.css';
+import classes from './page.module.css';
 import Link from 'next/link';
 
 export default function Home() {
     return (
-        <main>
-            <h1>Time to get started</h1>
-            <p>
-                <Link href="/meals">Meals</Link>
-            </p>
-            <p>
-                <Link href="/meals/share">Share Meals</Link>
-            </p>
-            <p>
-                <Link href="/community">Community</Link>
-            </p>
-        </main>
+        <>
+            <header className={classes.header}>
+                <div className={classes.slideshow}></div>
+                <div>
+                    <div className={classes.hero}>
+                        <h1>Next Food Up for your Next meal</h1>
+                        <p>Share your meal with your community</p>
+                    </div>
+
+                    <div className={classes.cta}>
+                        <Link href="/community">Join The Community</Link>
+                        <Link href="/meals">Explore Meals</Link>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <section className={classes.section}>
+                    <h2>How it works</h2>
+                    <p>
+                        Next Food Up is a platform for foodies to share their
+                        favorite recipes with the world. It&apos;s a place to
+                        discover new dishes, and to connect with other food
+                        lovers.
+                    </p>
+                    <p>
+                        Next Food up is a place to discover new dishes, and to
+                        connect with other food lovers.
+                    </p>
+                </section>
+            </main>
+        </>
     );
 }
