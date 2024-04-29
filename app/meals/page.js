a8ae3a2af3f9d7412c1578c22edbee98d416832e
Justin Dumadag <justin.dumadag@gmail.com>
2024-04-29 17:37:36 -0400

    DB work and adding new items

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,16 @@
 import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
-export default function Meals() {
+async function Meals() {
+    const meals = await getMeals();
+
+    return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
     return (
         <>
             <header className={classes.header}>
@@ -16,7 +24,13 @@ export default function Meals() {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={[]} />
+                <Suspense
+                    fallback={
+                        <p className={classes.loading}>Loading Meals...</p>
+                    }
+                >
+                    <Meals />
+                </Suspense>
             </main>
         </>
     );
