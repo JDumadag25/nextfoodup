6d862326cfb8200c6003a4ddd055f1374a8c5873
Justin Dumadag <justin.dumadag@gmail.com>
2024-04-29 17:52:14 -0400

    validation and error message

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,14 @@
+'use client';
+
+import { useFormState } from 'react-dom';
 import classes from './page.module.css';
 import ImagePicker from '@/components/image-picker/image-picker';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import { shareMeal } from '@/lib/actions';
+
 export default function ShareMealPage() {
+    const [state, formAction] = useFormState(shareMeal, { message: null });
+
     return (
         <>
             <header className={classes.header}>
@@ -13,7 +19,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form} action={shareMeal}>
+                <form className={classes.form} action={formAction}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -52,6 +58,7 @@ export default function ShareMealPage() {
                         ></textarea>
                     </p>
                     <ImagePicker lable="Your Image" name="image" />
+                    {state.message && <p>{state.message}</p>}
                     <p className={classes.actions}>
                         <MealsFormSubmit />
                     </p>
